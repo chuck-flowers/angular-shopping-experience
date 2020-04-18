123504d8e32eec508d1866d0e30ac694436ab5c4
Timothy Flowers <t_chuck_flowers@yahoo.com>
2020-04-18 12:34:07 -0400

    Adds the product browser to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductBrowserComponent } from './product-browser/product-browser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductBrowserComponent
   ],
   imports: [
     BrowserModule,
