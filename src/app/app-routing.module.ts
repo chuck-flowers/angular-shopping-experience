ebb6f5a958d0ecb5bf1a1dac5e96b51278b0fb9f
Timothy Flowers <t_chuck_flowers@yahoo.com>
2020-04-18 12:37:45 -0400

    Adds the product browser to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductBrowserComponent } from './product-browser/product-browser.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/', component: ProductBrowserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
